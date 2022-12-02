6ac4be16891db7af892cf48892cd6b1611d67337
Gr871 <ky3971@yandex.ru>
2022-12-02 21:47:35 +0300

    local tests with secrets completed

--- firebase_admin/src/servers/controllers/secrets-controller.ts
+++ firebase_admin/src/servers/controllers/secrets-controller.ts
@@ -1,5 +1,23 @@
+import {Controller, Get, Param} from "routing-controllers";
+import {EnvVar} from "../../utils/environment-variables";
+import * as fs from "fs";
 
-
+@Controller("/secrets")
 export class SecretsController {
 
+    @Get('/')
+    isAlive(){
+       return "Secret controller is alive"
+    }
+
+    @Get('/:id')
+    getSecret(@Param('id') secretKey: string) {
+        return EnvVar.getString(secretKey);
+    }
+
+    @Get('/files/:id')
+    getFileSecret(@Param('id') path: string) {
+        const result = fs.readFileSync(EnvVar.getString(path), 'utf-8')
+        return result;
+    }
 }
